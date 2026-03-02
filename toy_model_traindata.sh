2f641da52472abae467e6e1597f81fe4e952fa06
Jamie Taylor <j.taylor.1990@gmail.com>
2026-03-02 00:51:06 +0000

    toy trainset

--- toy_model_traindata.sh
+++ toy_model_traindata.sh
@@ -16,7 +16,7 @@ VALUE_MODE="winloss"
 target/release/precompute \
   --export data/exports/traj_train_spe_soft_exact.jsonl \
   --export-mode trajectory \
-  --games 4000 \
+  --games 2000 \
   --seed 20260301 \
   --threads $THREADS --chunk-size $CHUNK \
   --hand-strategy stratified \
@@ -30,7 +30,7 @@ target/release/precompute \
 target/release/precompute \
   --export data/exports/traj_val_spe_soft_exact.jsonl \
   --export-mode trajectory \
-  --games 1000 \
+  --games 500 \
   --seed 20260302 \
   --threads $THREADS --chunk-size $CHUNK \
   --hand-strategy stratified \
@@ -44,7 +44,7 @@ target/release/precompute \
 target/release/precompute \
   --export data/exports/traj_test_spe_soft_exact.jsonl \
   --export-mode trajectory \
-  --games 1000 \
+  --games 500 \
   --seed 20260303 \
   --threads $THREADS --chunk-size $CHUNK \
   --hand-strategy stratified \
